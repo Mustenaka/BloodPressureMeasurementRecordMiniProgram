893795ea398c518166a592bdf34bc9387d472242
Andrew·Redfiled·Wong <31812641+Mustenaka@users.noreply.github.com>
2022-05-05 18:00:55 +0800

    Init Commit

---
+++ pages/myself/myself.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		this is a personal page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
