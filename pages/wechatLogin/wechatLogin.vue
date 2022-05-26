e2c6d5d2acd3f54e0939c42c96bcd5c6f2b8c603
Andrew·Redfiled·Wong <31812641+Mustenaka@users.noreply.github.com>
2022-05-26 11:10:39 +0800

    微信快捷注册、登录按钮完成

--- pages/wechatLogin/wechatLogin.vue
+++ pages/wechatLogin/wechatLogin.vue
@@ -1,8 +1,9 @@
 <template>
 	<view class="content">
-		<text>微信登录</text>
+		<h1>微信登录</h1>
 		<view class="text-area">
-			<button style="margin-top: 200rpx;" type="primary" @click="login">微信登录</button>
+			<button class="button-style" type="primary" @click="login">微信登录</button>
+			<button class="button-style" type="primary" @click="register">微信注册</button>
 		</view>
 	</view>
 </template>
@@ -10,11 +11,14 @@
 <script>
 	export default {
 		data() {
-			return {
-
-			}
+			return { }
+		},
+		created() {
+			console.log("create触发")
+			this.tester()
 		},
 		methods: {
+			// 登录方法
 			login() {
 				uni.getUserProfile({
 					// 这个参数是必须的，desc属性,（声明获取用户信息后的用途）后续会展示在弹窗中
@@ -34,7 +38,8 @@
 
 								// 获取code
 								var code = res2.code
-								console.log("用户code获取成功:" + JSON.stringify(res2))
+								console.log("用户code获取:" + JSON.stringify(res2))
+								console.log("UserInfo:", JSON.stringify(userInfo))
 
 								// step3: 向后端服务器发送请求
 								uni.request({
@@ -51,10 +56,10 @@
 									// 把数据发送给后端
 									success: (res3) => {
 										// 登录成果
-										console.log('登陆成功:' + JSON.stringify(res3
+										console.log('登陆:' + JSON.stringify(res3
 											.data));
 
-										// 登录成功
+										// 登录成功 code = 0
 										if (res3.data.err_code == 0) {
 											// 存储token
 											var token = res3.data.data.token
@@ -72,21 +77,19 @@
 											});
 										} else if (res3.data.err_code == 40003) {
 											// 登陆失败，没有该用户 - 执行自动注册程序
-											uni.request({
-												url: 'http://1.117.222.119/wxregister',
-												method: 'POST',
-												header: {
-													"content-type": "application/json",
-												},
-												data: {
-													"code": code
-												},
+											
+											// 隐藏加载界面
+											uni.hideLoading();
+											
+											uni, uni.showToast({
+												title: "登陆失败-请先注册",
+												icon: 'none'
 											})
 										} else {
 											// 登陆失败
 											uni.hideLoading();
 											uni, uni.showToast({
-												title: "登陆失败",
+												title: "登陆失败-可能是网络问题",
 												icon: 'none'
 											})
 										}
@@ -103,6 +106,75 @@
 						})
 					}
 				})
+			},
+
+			// 注册方法
+			register() {
+				uni.getUserProfile({
+					desc: '第一次使用自动注册',
+					success: (res) => {
+						// step1. 用户点击了允许，拿到了用户信息
+						console.log("用户信息: " + JSON.stringify(res.userInfo))
+						let userInfo = res.userInfo
+
+						// step2. 调用uni.login(). 获取code
+						uni.login({
+							provider: 'weixin',
+							success: (res2) => {
+								uni.showLoading({
+									title: '注册中'
+								})
+
+								// 获取code
+								var code = res2.code
+								console.log("用户code获取成功:" + JSON.stringify(res2))
+								console.log("UserInfo:", JSON.stringify(userInfo))
+
+								// 登陆失败，没有该用户 - 执行自动注册程序
+								uni.request({
+									url: 'http://1.117.222.119/wxregister',
+									method: 'POST',
+									header: {
+										"content-type": "application/json",
+									},
+
+									data: {
+										"code": code,
+										"username": userInfo.nickName,
+										"sex": (userInfo.gender == 0) ? "男" : "女",
+										"avatarUrl": userInfo.avatarUrl
+									},
+
+									success: (res3) => {
+										console.log("注册:" +
+											JSON.stringify(
+												res3.data));
+										
+										// 隐藏加载界面
+										uni.hideLoading();
+										
+										uni, uni.showToast({
+											title: "注册成功",
+											icon: 'none'
+										})
+									},
+									fail: (err) => {
+										uni.hideLoading()
+										uni, uni.showToast({
+											title: "注册失败",
+											icon: 'none'
+										})
+									},
+								})
+							}
+						})
+					}
+				})
+			},
+		
+			// 测试方法
+			tester(){
+				console.log("target tester!");
 			}
 		}
 	}
@@ -112,7 +184,33 @@
 	.content {
 		display: flex;
 		flex-direction: column;
-		/* align-items: center; */
+		align-items: center;
 		justify-content: center;
+	}
+	
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+	
+	.text-area {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.button-style {
+/* 		height: 300rpx;
+		width: 200rpx; */
+		margin-top: 200rpx;
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/wechatLogin/wechatLogin.js
+++ unpackage/dist/dev/mp-weixin/pages/wechatLogin/wechatLogin.js
@@ -4,13 +4,17 @@ const _sfc_main = {
   data() {
     return {};
   },
+  created() {
+    console.log("create\u89E6\u53D1");
+    this.tester();
+  },
   methods: {
     login() {
       common_vendor.index.getUserProfile({
         desc: "\u5FAE\u4FE1\u5FEB\u6377\u767B\u5F55",
         success: (res) => {
           console.log("\u7528\u6237\u4FE1\u606F: " + JSON.stringify(res.userInfo));
-          res.userInfo;
+          let userInfo = res.userInfo;
           common_vendor.index.login({
             provider: "weixin",
             success: (res2) => {
@@ -18,7 +22,8 @@ const _sfc_main = {
                 title: "\u767B\u9646\u4E2D"
               });
               var code = res2.code;
-              console.log("\u7528\u6237code\u83B7\u53D6\u6210\u529F:" + JSON.stringify(res2));
+              console.log("\u7528\u6237code\u83B7\u53D6:" + JSON.stringify(res2));
+              console.log("UserInfo:", JSON.stringify(userInfo));
               common_vendor.index.request({
                 url: "http://1.117.222.119/wxlogin",
                 method: "POST",
@@ -29,7 +34,7 @@ const _sfc_main = {
                   "code": code
                 },
                 success: (res3) => {
-                  console.log("\u767B\u9646\u6210\u529F:" + JSON.stringify(res3.data));
+                  console.log("\u767B\u9646:" + JSON.stringify(res3.data));
                   if (res3.data.err_code == 0) {
                     var token = res3.data.data.token;
                     common_vendor.index.setStorageSync("token", token);
@@ -40,20 +45,15 @@ const _sfc_main = {
                       animationDuration: 200
                     });
                   } else if (res3.data.err_code == 40003) {
-                    common_vendor.index.request({
-                      url: "http://1.117.222.119/wxregister",
-                      method: "POST",
-                      header: {
-                        "content-type": "application/json"
-                      },
-                      data: {
-                        "code": code
-                      }
+                    common_vendor.index.hideLoading();
+                    common_vendor.index.showToast({
+                      title: "\u767B\u9646\u5931\u8D25-\u8BF7\u5148\u6CE8\u518C",
+                      icon: "none"
                     });
                   } else {
                     common_vendor.index.hideLoading();
                     common_vendor.index.showToast({
-                      title: "\u767B\u9646\u5931\u8D25",
+                      title: "\u767B\u9646\u5931\u8D25-\u53EF\u80FD\u662F\u7F51\u7EDC\u95EE\u9898",
                       icon: "none"
                     });
                   }
@@ -70,12 +70,64 @@ const _sfc_main = {
           });
         }
       });
+    },
+    register() {
+      common_vendor.index.getUserProfile({
+        desc: "\u7B2C\u4E00\u6B21\u4F7F\u7528\u81EA\u52A8\u6CE8\u518C",
+        success: (res) => {
+          console.log("\u7528\u6237\u4FE1\u606F: " + JSON.stringify(res.userInfo));
+          let userInfo = res.userInfo;
+          common_vendor.index.login({
+            provider: "weixin",
+            success: (res2) => {
+              common_vendor.index.showLoading({
+                title: "\u6CE8\u518C\u4E2D"
+              });
+              var code = res2.code;
+              console.log("\u7528\u6237code\u83B7\u53D6\u6210\u529F:" + JSON.stringify(res2));
+              console.log("UserInfo:", JSON.stringify(userInfo));
+              common_vendor.index.request({
+                url: "http://1.117.222.119/wxregister",
+                method: "POST",
+                header: {
+                  "content-type": "application/json"
+                },
+                data: {
+                  "code": code,
+                  "username": userInfo.nickName,
+                  "sex": userInfo.gender == 0 ? "\u7537" : "\u5973",
+                  "avatarUrl": userInfo.avatarUrl
+                },
+                success: (res3) => {
+                  console.log("\u6CE8\u518C:" + JSON.stringify(res3.data));
+                  common_vendor.index.hideLoading();
+                  common_vendor.index.showToast({
+                    title: "\u6CE8\u518C\u6210\u529F",
+                    icon: "none"
+                  });
+                },
+                fail: (err) => {
+                  common_vendor.index.hideLoading();
+                  common_vendor.index.showToast({
+                    title: "\u6CE8\u518C\u5931\u8D25",
+                    icon: "none"
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    },
+    tester() {
+      console.log("target tester!");
     }
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: common_vendor.o((...args) => $options.login && $options.login(...args))
+    a: common_vendor.o((...args) => $options.login && $options.login(...args)),
+    b: common_vendor.o((...args) => $options.register && $options.register(...args))
   };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/work/Go/BloodPressureMeasurementRecordMiniProgram/pages/wechatLogin/wechatLogin.vue"]]);
