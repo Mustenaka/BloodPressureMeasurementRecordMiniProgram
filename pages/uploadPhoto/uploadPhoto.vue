353016f1b78786c3d8ea90cf30227d630500fd8f
Andrew·Redfiled·Wong <31812641+Mustenaka@users.noreply.github.com>
2022-06-06 21:50:27 +0800

    上传照片，舌苔脉象 假接口

--- pages/uploadPhoto/uploadPhoto.vue
+++ pages/uploadPhoto/uploadPhoto.vue
@@ -1,6 +1,30 @@
 <template>
-	<view>
-		
+	<view class="home">
+		<!-- 首部轮播图 -->
+		<view class="header">
+			<text class="header-text">照片上传</text>
+		</view>
+
+		<!-- 中部选择框 -->
+		<view class="box">
+			<uni-card :is-shadow="false" is-full>
+				<text class="uni-h6">文件选择上传组件，可以选择图片、视频等任意文件并上传到当前绑定的服务空间。</text>
+			</uni-card>
+			<uni-section title="只选择图片" type="line">
+				<view class="example-body">
+					<uni-file-picker limit="9" title="最多选择9张图片"></uni-file-picker>
+				</view>
+			</uni-section>
+
+			<view class="item">
+				<button type="primary" @click="submit" disabled="true">提交照片</button>
+			</view>
+		</view>
+
+		<!-- 尾部空白框，计划留作公司信息展示框 -->
+		<view class="box" style="margin-top: 30px;">
+			<text class="slogan">龙医护心 - 守护您的健康</text>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +32,92 @@
 	export default {
 		data() {
 			return {
-				
-			}
-		},
-		methods: {
-			
+
+			};
 		}
 	}
 </script>
 
-<style>
 
+<style lang="scss">
+	.home {
+		.body {
+			padding: 0 30rpx;
+			background-color: #ECECEC;
+		}
+
+		.header {
+			background-color: #6FAD8F;
+			padding: 25rpx;
+			padding-bottom: 60rpx;
+
+			height: 250rpx;
+			width: 700rpx;
+		}
+
+		.header-text {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+			font-size: 48rpx;
+		}
+
+		.body {
+			padding: 0 30rpx;
+			background-color: #ECECEC;
+		}
+
+		.example-body {
+			background-color: #fff;
+			padding: 10px;
+		}
+
+		.box {
+			background-color: #fff;
+			border-radius: 25rpx;
+
+			margin: 0 30rpx;
+			margin-top: -200rpx;
+
+			flex-wrap: wrap;
+			justify-content: center;
+
+			width: 700rpx;
+			padding: 60rpx 25rpx;
+
+			box-shadow: 9px 10px 16px rgba(0, 0, 0, 0.5);
+			box-sizing: border-box;
+		}
+
+		.example-body {
+			padding: 10px;
+			padding-top: 0;
+		}
+
+		.form-item {}
+
+		.item {
+			width: 98%;
+
+			box-sizing: border-box;
+
+			margin: 2rpx 0;
+			padding: 10rpx;
+
+			align-items: center;
+			text-align: left;
+
+			flex-direction: row;
+
+			font-weight: 550;
+			font-size: 80%;
+
+		}
+
+		.slogan {
+			font-size: 9pt;
+			text-align: center;
+			letter-spacing: 12px;
+		}
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/uploadPhoto/uploadPhoto.js
+++ unpackage/dist/dev/mp-weixin/pages/uploadPhoto/uploadPhoto.js
@@ -3,11 +3,35 @@ var common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {};
-  },
-  methods: {}
+  }
 };
+if (!Array) {
+  const _easycom_uni_card2 = common_vendor.resolveComponent("uni-card");
+  const _easycom_uni_file_picker2 = common_vendor.resolveComponent("uni-file-picker");
+  const _component_uni_section = common_vendor.resolveComponent("uni-section");
+  (_easycom_uni_card2 + _easycom_uni_file_picker2 + _component_uni_section)();
+}
+const _easycom_uni_card = () => "../../uni_modules/uni-card/components/uni-card/uni-card.js";
+const _easycom_uni_file_picker = () => "../../uni_modules/uni-file-picker/components/uni-file-picker/uni-file-picker.js";
+if (!Math) {
+  (_easycom_uni_card + _easycom_uni_file_picker)();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.p({
+      ["is-shadow"]: false,
+      ["is-full"]: true
+    }),
+    b: common_vendor.p({
+      limit: "9",
+      title: "\u6700\u591A\u9009\u62E99\u5F20\u56FE\u7247"
+    }),
+    c: common_vendor.p({
+      title: "\u53EA\u9009\u62E9\u56FE\u7247",
+      type: "line"
+    }),
+    d: common_vendor.o((...args) => _ctx.submit && _ctx.submit(...args))
+  };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/work/Go/BloodPressureMeasurementRecordMiniProgram/pages/uploadPhoto/uploadPhoto.vue"]]);
 wx.createPage(MiniProgramPage);
