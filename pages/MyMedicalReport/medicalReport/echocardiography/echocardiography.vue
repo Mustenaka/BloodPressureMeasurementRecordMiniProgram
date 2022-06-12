feeea710ca1280b9276d2077df8e9428ec897a92
Andrew·Redfiled·Wong <31812641+Mustenaka@users.noreply.github.com>
2022-06-12 17:32:36 +0800

    【添加】我的检查报告-心超

--- pages/MyMedicalReport/medicalReport/echocardiography/echocardiography.vue
+++ pages/MyMedicalReport/medicalReport/echocardiography/echocardiography.vue
@@ -1,6 +1,27 @@
 <template>
-	<view>
-		
+	<view class="home">
+		<!-- 首部轮播图 -->
+		<view class="header">
+			<text class="header-text">我的心超记录</text>
+		</view>
+
+		<view class="box">
+			<!-- 中部选择框 -->
+			<view class="item" v-for="(value,key,index) in this.Array" :key="key">
+				<view class="item_detail">
+					<text class="text_left">记录{{index+1}} </text>
+					<text>创建时间:{{value.create_at}}</text>
+				</view>
+				<view class="item_detail">
+					<text class="text_right">EF:{{value.ef}}(%) LVIDd:{{value.lvidd}}(mm) LVIDs:{{value.lvids}}(mm)</text>
+				</view>
+			</view>
+
+			<view class="item">
+				<button type="primary" @click="submit">新增心超记录</button>
+			</view>
+		</view>
+
 	</view>
 </template>
 
@@ -8,15 +29,111 @@
 	export default {
 		data() {
 			return {
-				
-			}
+				Array: []
+			};
 		},
+
+		onLoad() {
+			this.getData();
+		},
+
 		methods: {
-			
+			getData() {
+				const url = 'http://1.117.222.119/v1/user/mrechocardiographys?limit_count=0'
+				this.$http.sendRequest(url, 'GET', {})
+					.then(
+						res => {
+							//成功回调
+							console.log(res);
+
+							// 循环遍历拿数据并清洗
+							var Array = res.data.data;
+							Array.forEach((item, index, arr) => {
+								item.create_at = item.create_at.slice(0, 10);
+							});
+
+							this.Array = Array;
+							console.log(this.Array);
+						}).catch(err => {
+						//请求失败
+						console.log(err);
+					})
+			},
+
+			submit() {
+				uni.navigateTo({
+					//保留当前页面，跳转到应用内的某个页面
+					url: 'echocardiographyRecord',
+					animationType: "pop-in",
+					animationDuration: 200,
+				});
+			}
 		}
 	}
 </script>
 
-<style>
 
+<style lang="scss">
+	.home {
+		.header {
+			background-color: #6FAD8F;
+			padding: 25rpx;
+			padding-bottom: 60rpx;
+
+			height: 250rpx;
+			width: 700rpx;
+		}
+
+		.header-text {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+			font-size: 48rpx;
+		}
+
+		.box {
+			background-color: #fff;
+			border-radius: 25rpx;
+
+			margin: 0 30rpx;
+			margin-top: -200rpx;
+
+			// display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+
+			width: 700rpx;
+			// height: 85vh;
+			padding: 60rpx 25rpx;
+
+			box-shadow: 9px 10px 16px rgba(0, 0, 0, 0.5);
+			box-sizing: border-box;
+		}
+
+		.item {
+			width: 98%;
+
+			box-sizing: border-box;
+			margin: 2rpx 0;
+			padding: 10rpx;
+
+			// align-items: center;
+			// text-align: left;
+			flex-direction: row;
+
+			font-weight: 550;
+			font-size: 90%;
+		}
+
+		.item_detail {
+			width: 98%;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+		}
+
+		.slogan {
+			font-size: 9pt;
+		}
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/MyMedicalReport/medicalReport/echocardiography/echocardiographyRecord.js
+++ unpackage/dist/dev/mp-weixin/pages/MyMedicalReport/medicalReport/echocardiography/echocardiographyRecord.js
@@ -2,12 +2,105 @@
 var common_vendor = require("../../../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      record: {
+        ef: 0,
+        lvidd: 0,
+        lvids: 0,
+        createAt: ""
+      }
+    };
   },
-  methods: {}
+  methods: {
+    dataEFChange(value) {
+      this.record.ef = value;
+    },
+    dataLVIDdChange(value) {
+      this.record.lvidd = value;
+    },
+    dataLVIDsChange(value) {
+      this.record.lvids = value;
+    },
+    submit() {
+      console.log("\u6570\u636E" + this.record.data);
+      this.$http.sendRequest("http://1.117.222.119/v1/user/mrechocardiographys", "POST", {
+        "ef": this.record.ef,
+        "lvidd": this.record.lvidd,
+        "lvids": this.record.lvids,
+        "create_at": this.record.createAt
+      }).then((res) => {
+        console.log(res);
+        common_vendor.index.showToast({
+          title: "\u5F55\u5165\u6210\u529F",
+          icon: "none"
+        });
+      }).catch((err) => {
+        console.log(err);
+        common_vendor.index.showToast({
+          title: "\u8BF7\u6C42\u5931\u8D25",
+          icon: "none"
+        });
+      });
+    }
+  }
 };
+if (!Array) {
+  const _component_uni_section = common_vendor.resolveComponent("uni-section");
+  const _easycom_uni_datetime_picker2 = common_vendor.resolveComponent("uni-datetime-picker");
+  const _easycom_uni_number_box2 = common_vendor.resolveComponent("uni-number-box");
+  (_component_uni_section + _easycom_uni_datetime_picker2 + _easycom_uni_number_box2)();
+}
+const _easycom_uni_datetime_picker = () => "../../../../uni_modules/uni-datetime-picker/components/uni-datetime-picker/uni-datetime-picker.js";
+const _easycom_uni_number_box = () => "../../../../uni_modules/uni-number-box/components/uni-number-box/uni-number-box.js";
+if (!Math) {
+  (_easycom_uni_datetime_picker + _easycom_uni_number_box)();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.p({
+      title: "\u65E5\u671F\u65F6\u95F4\uFF1A" + $data.record.createAt,
+      type: "line"
+    }),
+    b: common_vendor.o(($event) => $data.record.createAt = $event),
+    c: common_vendor.p({
+      type: "date",
+      modelValue: $data.record.createAt
+    }),
+    d: common_vendor.o($options.dataEFChange),
+    e: common_vendor.p({
+      value: $data.record.ef,
+      min: 0,
+      max: 5e4
+    }),
+    f: common_vendor.p({
+      title: "\u4F4E\u538B\u8BB0\u5F55: " + $data.record.ef,
+      type: "line",
+      padding: true
+    }),
+    g: common_vendor.o($options.dataLVIDdChange),
+    h: common_vendor.p({
+      value: $data.record.lvidd,
+      min: 0,
+      max: 5e4
+    }),
+    i: common_vendor.p({
+      title: "\u4F4E\u538B\u8BB0\u5F55: " + $data.record.lvidd,
+      type: "line",
+      padding: true
+    }),
+    j: common_vendor.o($options.dataLVIDsChange),
+    k: common_vendor.p({
+      value: $data.record.lvids,
+      min: 0,
+      max: 5e4
+    }),
+    l: common_vendor.p({
+      title: "\u4F4E\u538B\u8BB0\u5F55: " + $data.record.lvids,
+      type: "line",
+      padding: true
+    }),
+    m: common_vendor.o((...args) => $options.submit && $options.submit(...args))
+  };
 }
 var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/work/Go/BloodPressureMeasurementRecordMiniProgram/pages/MyMedicalReport/medicalReport/echocardiography/echocardiographyRecord.vue"]]);
 wx.createPage(MiniProgramPage);
